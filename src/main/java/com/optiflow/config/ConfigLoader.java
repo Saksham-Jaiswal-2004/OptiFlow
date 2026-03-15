14b9750c7e29bfe15fc9422df9dfe1a0e200c956
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2026-03-15 12:10:01 +0530

    Config Properties added for Secret Data

---
+++ src/main/java/com/optiflow/config/ConfigLoader.java
@@ -0,0 +1,29 @@
+package com.optiflow.config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader
+{
+
+    private static Properties properties = new Properties();
+
+    static
+    {
+        try {
+            InputStream input = ConfigLoader.class
+                    .getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            properties.load(input);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key)
+    {
+        return properties.getProperty(key);
+    }
+}
--- src/main/java/com/optiflow/database/DBConnection.java
+++ src/main/java/com/optiflow/database/DBConnection.java
@@ -2,6 +2,7 @@ package com.optiflow.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import com.optiflow.config.ConfigLoader;
 
 public class DBConnection
 {
@@ -11,11 +12,11 @@ public class DBConnection
 
         try
         {
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/optiflow",
-                    "root",
-                    "iwillnottell"
-            );
+            String url = ConfigLoader.getProperty("db.url");
+            String user = ConfigLoader.getProperty("db.username");
+            String password = ConfigLoader.getProperty("db.password");
+
+            return DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
             e.printStackTrace();
         }
