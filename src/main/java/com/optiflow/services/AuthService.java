b04a9ac9d87cfdb7706266a89d08e495dc307d93
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2026-03-21 00:54:05 +0530

    AuthService - Login Function

--- src/main/java/com/optiflow/services/AuthService.java
+++ src/main/java/com/optiflow/services/AuthService.java
@@ -1,14 +1,38 @@
 package com.optiflow.services;
 
+import com.optiflow.dao.UserDAO;
 import com.optiflow.models.User;
+
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class AuthService
 {
-    public User login(String email, String password)
-    {
-        User u1 = new User();
+    private UserDAO userDAO;
 
-        return u1;
+    public AuthService()
+    {
+        this.userDAO = new UserDAO();
+    }
+
+    public User login(String email, String password) throws SQLException
+    {
+        User user = userDAO.getUserByEmail(email);
+
+        if(user == null)
+        {
+            System.out.println("User with this email does not exist!");
+            return null;
+        }
+
+        if(password.equals(user.getPasswordHash()))
+        {
+            System.out.println("Login Successful!");
+            return user;
+        }
+
+        System.out.println("Invalid Login Credentials!");
+        return user;
     }
 
     public boolean register(User user)
