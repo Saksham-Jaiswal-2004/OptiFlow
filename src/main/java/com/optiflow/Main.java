18beee306e67bc6f05cdfeeff7dff6a50c9547d1
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2026-04-03 11:44:26 +0530

    New Auth Pages UI Added

--- src/main/java/com/optiflow/Main.java
+++ src/main/java/com/optiflow/Main.java
@@ -1,20 +1,22 @@
 package com.optiflow;
 
+import com.optiflow.database.DBConnection;
 import com.optiflow.utils.AppContext;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import com.optiflow.database.DBConnection;
-import java.sql.*;
+
+import java.sql.Connection;
 
 public class Main extends Application
 {
     @Override
     public void start(Stage stage) throws Exception
     {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/Register.fxml"));
         if (loader.getLocation() == null)
         {
             throw new RuntimeException("FXML file not found!");
@@ -22,25 +24,37 @@ public class Main extends Application
 
         Scene scene = new Scene(loader.load());
 
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
         stage.setTitle("OptiFlow");
         stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.setResizable(false);
         stage.show();
+
+        if(AppContext.getSocketClient() != null)
+        {
+            AppContext.getSocketClient().setListener(message ->
+            {
+                Platform.runLater(() -> {
+                    System.out.println(message.getMessageType());
+                });
+            });
+        }
     }
 
     public static void main(String[] args) throws Exception
     {
-        Connection conn = DBConnection.getConnection();
-        if(conn != null)
-            System.out.println("DATABASE CONNECTED!");
-        else
-            System.out.println("FAILED TO CONNECT DATABASE!");
-        launch();
-
-        AppContext.getSocketClient().setListener(message ->
+        try(Connection conn = DBConnection.getConnection())
         {
-            Platform.runLater(() -> {
-                System.out.println(message.getMessageType());
-            });
-        });
+            if (conn != null)
+                System.out.println("DATABASE CONNECTED!");
+            else
+                System.out.println("FAILED TO CONNECT DATABASE!");
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+
+        launch();
     }
 }
